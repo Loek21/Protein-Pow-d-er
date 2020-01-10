c21b5e30564218f0e3fb2d64f0f34bae57b54fb4
Kruxin <sebaskruize@gmail.com>
2020-01-10 09:48:58 +0100

    matrix now callable n times

--- main.py
+++ main.py
@@ -63,25 +63,30 @@ if __name__ == '__main__':
     test_lattice.load_list()
 
     if data_structure == "matrix":
-        random_matrix = randomizematrix.matrixrandomizer(test_lattice, TwoD_moves)
+        for _ in range(iterations):
+            random_matrix = randomizematrix.matrixrandomizer(test_lattice, TwoD_moves)
+            stability = randomizematrix.matrix_stability(test_lattice)
+            print(stability)
+            test_lattice = lattice.Lattice(protein_string)
+            test_lattice.load_matrix()
+            test_lattice.load_list()
+            # for row in range(len(random_matrix)):
+            #     for element in range(len(random_matrix)):
+            #         if random_matrix[row][element] == None:
+            #             random_matrix[row][element] = 0.0
 
-        for row in range(len(random_matrix)):
-            for element in range(len(random_matrix)):
-                if random_matrix[row][element] == None:
-                    random_matrix[row][element] = 0.0
+            #         elif random_matrix[row][element].type == 'H':
+            #             random_matrix[row][element] = 5.0
 
-                elif random_matrix[row][element].type == 'H':
-                    random_matrix[row][element] = 5.0
+            #         else:
+            #             random_matrix[row][element] = 10.0
 
-                else:
-                    random_matrix[row][element] = 10.0
+            # new_matrix = np.zeros((len(random_matrix), len(random_matrix)))
+            # for row in range(len(random_matrix)):
+            #     for element in range(len(random_matrix)):
+            #         new_matrix[row][element] = random_matrix[row][element]
 
-        new_matrix = np.zeros((len(random_matrix), len(random_matrix)))
-        for row in range(len(random_matrix)):
-            for element in range(len(random_matrix)):
-                new_matrix[row][element] = random_matrix[row][element]
-
-        visualise.matrix_plot(new_matrix)
+            # visualise.matrix_plot(new_matrix)
 
     if data_structure == "dict":
         best_stability = 1
